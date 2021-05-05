c870bbb7b38c6efb48a46722739c1f4178a439a4
Nikulin Yury <nikulin.yura@gmail.com>
2021-05-05 12:18:48 +0300

    add comments

--- algorithmes/Binare_search/binary_seach.py
+++ algorithmes/Binare_search/binary_seach.py
@@ -1,21 +1,34 @@
-# бинарный поиск алгоритм
-# add comments
+# binary search
 
-def binary(lst, item):
-    low = 0
-    high = len(lst)-1
-    while low < high:
-        mid = int(low+high)/2
-        g = lst[int(mid)]
-        if g == item:
-            return g
-        if g < item:
-            low = mid+1
-        else:
-            high = mid-1
+
+# ONLY WITH SORTED LISTS
+
+# searching the possition(index) in sorted list
+
+def binary(lst, item):                      # add func lst - sorted list, item  - item in lst
+    low = 0                                 # minimum index
+    high = len(lst)-1                       # maximum index
+    
+    while low <= high:                      # while  more then 1 indexes
+        
+        mid = (low+high)//2                 # middle index
+        g = lst[mid]                        # g =  item with middle index in list   !!! 
+        
+        if g == item:                       # item has been searched
+            return mid
+        
+        if g < item:                        # guess item less then item
+            low = mid+1                     # lowest index is middle index
+        
+        if g > item:                        # guedd item more then item
+            high = mid-1                    # highest index is middle index
+    
     return None
 
-ml = [x for x in range(1, 10, 2)]
+
+# TEST
+ml = [x for x in range(1, 100, 2)]          # sorted list
+print (ml)
 
 print(binary(ml, 5))  
-print(binary(ml, 2))  
+print(binary(ml, 87))  
