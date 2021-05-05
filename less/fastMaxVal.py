8fa0c267250c84af7a6f5188370021c548dba2fb
Nikulin Yury <nikulin.yura@gmail.com>
2021-05-05 11:34:22 +0300

    add folders

--- less/fastMaxVal.py
+++ less/fastMaxVal.py
@@ -1,6 +1,10 @@
+
+
 def fastmaxVal(toConsider, avail, memo = {}):
+
     if (len(toConsider), avail) in memo:
         result = memo[(len(toConsider), avail)]
+        
     elif toConsider == [] or avail == 0:
         result = (0, ())
     # elif toConsider[0].
