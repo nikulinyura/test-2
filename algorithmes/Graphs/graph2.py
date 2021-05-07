35ab8861200c0baaf0c71bd10416a9c570df0260
nikulinyura <nikulin.yura@gmail.com>
2021-05-07 14:27:33 +0300

    like def

--- algorithmes/Graphs/graph2.py
+++ algorithmes/Graphs/graph2.py
@@ -1,5 +1,3 @@
-
-#test
 g = {}
 g['cab'] = ['car', 'cat']
 g['car'] = ['cat', 'bar']
@@ -7,25 +5,25 @@ g['cat'] = ['mat', 'bat']
 g['bar'] = ['bat']
 g['mat'] = ['bat']
 
-'''    
-from collections import deque
-sq = deque()
-sq += g['cab']
-itm = sq.popleft()
+def sp(dic, s, f):
+    from collections import deque
+    sq = deque()
+    sq += dic[s]
+    p=1
 
-while sq:
-    if fin(itm):
-        print(itm + 'IT IS')
-    else:
-        sq +=g[itm]
+    while sq:
+        i = sq.popleft()
+        if i == f:
+            return s, 'to', f, 'is', p, 'step'
+            
+        else:
+            if not sq:
+                p+=1
+                try: sq +=dic[i]
+                except KeyboardInterrupt:
+                    print('no') 
+                    break
 
 
-def ser(s, f):
-    pass
-
-
-def fin(i):
-    if i == 'bat':
-        return True
-'''
-print('-' * 20)
+#test
+print(sp(g, 'mat', 'bat'))
