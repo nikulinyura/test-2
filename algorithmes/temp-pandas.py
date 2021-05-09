481ceeed53f500bc6f2e48039e04f1f2ffa2e431
Nikulin Yury <nikulin.yura@gmail.com>
2021-05-09 20:41:55 +0300

    g

--- algorithmes/temp-pandas.py
+++ algorithmes/temp-pandas.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+'''
 print('-' * 20)
+print('Series([5, 6, 7, 8, 9, 10])')
 my_series = pd.Series([5, 6, 7, 8, 9, 10])
-print(my_series[4:])
-
+print(my_series[0:])
+'''
 print('-' * 20)
+print("Series({'a': 5, 'b': 6, 'c': 7, 'd': 8})")
 my_series3 = pd.Series({'a': 5, 'b': 6, 'c': 7, 'd': 8})
 print(my_series3)
 
 
 print('-' * 20)
+print("DataFrame({'country': ['Kazakhstan', 'Russia', 'Belarus', 'Ukraine'], 'population': [17.04, 143.5, 9.5, 45.5],'square': [2724902, 17125191, 207600, 603628]}, index=['KZ', 'RU', 'BY', 'UA'])")
 df = pd.DataFrame({'country': ['Kazakhstan', 'Russia', 'Belarus', 'Ukraine'], 
 'population': [17.04, 143.5, 9.5, 45.5],
 'square': [2724902, 17125191, 207600, 603628]}, 
@@ -18,7 +21,7 @@ index=['KZ', 'RU', 'BY', 'UA'])
 df.index.name = 'ZIP'
 print (df)
 
-
+'''
 print('-' * 20)
 df.plot()
 plt.show()
@@ -26,3 +29,5 @@ plt.show()
 
 
 print('-' * 20)
+
+'''
