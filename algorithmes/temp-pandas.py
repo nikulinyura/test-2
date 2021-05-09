b21b3d52d47c0e5c484c36ab207067a32544d449
Nikulin Yury <nikulin.yura@gmail.com>
2021-05-09 20:50:41 +0300

    a

--- algorithmes/temp-pandas.py
+++ algorithmes/temp-pandas.py
@@ -14,6 +14,8 @@ print(my_series3)
 
 print('-' * 20)
 print("DataFrame({'country': ['Kazakhstan', 'Russia', 'Belarus', 'Ukraine'], 'population': [17.04, 143.5, 9.5, 45.5],'square': [2724902, 17125191, 207600, 603628]}, index=['KZ', 'RU', 'BY', 'UA'])")
+
+
 df = pd.DataFrame({'country': ['Kazakhstan', 'Russia', 'Belarus', 'Ukraine'], 
 'population': [17.04, 143.5, 9.5, 45.5],
 'square': [2724902, 17125191, 207600, 603628]}, 
