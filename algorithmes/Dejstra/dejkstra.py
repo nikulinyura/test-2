902f0b1059faedb2c3921e5ffdfd6714cb32f53b
nikulinyura <nikulin.yura@gmail.com>
2021-05-11 14:48:28 +0300

    jq

--- algorithmes/Dejstra/dejkstra.py
+++ algorithmes/Dejstra/dejkstra.py
@@ -1,36 +1,51 @@
 # add dicriptions!
 # make as function
+'''
+1) find target with min cost
+2) upgrade costs neighbors that node
+3) repeat it fo all nodes
+4) compute summary way
+'''
+
 
 import math
 math.inf 
+infinity = float(math.inf) # плюс бесконечность
 
+# graph hesh-tabl
 g ={}
 g['start'] = {}
 g['start']['a'] = 6
 g['start']['b'] = 2
 
 g['a'] ={}
+g['a']['fin'] = 1
+
 g['b'] = {}
 g['b']['a'] = 3
-g['a']['fin'] = 1
 g['b']['fin'] = 5
+
 g['fin'] = {}
 
-infinity = float(math.inf) # плюс бесконечность
-costs = {} # ключ цель, значение стоимость
+
+# cost hesh-tab
+costs = {} # ключ цель, значение стоимость. стоимость от старта до соседа
 costs['a'] = 6
 costs['b'] = 2
 costs['fin'] = infinity
 
+
+#perents hesh-tab
 pars = {}
 pars['a'] = 'start' # родитель в значении, цель в ключе
 pars['b'] = 'start'
 pars['fin'] = None
 
-processed = []
+processed = [] # processed  nodes list
+path = ['fin']
 
-def flc(costs): # ищем цель с наименьшей стоимостью
-    lcost = float(math.inf) # наменьшая стоимость - бесконечность
+def flc(costs): # ищем node с наименьшей стоимостью
+    lcost = float(math.inf) # наименьшая стоимость - бесконечность
     lcost_node = None # цель с наименьшей стоимостью
     
     for node in costs: # перебор ключей(целей)
@@ -42,18 +57,40 @@ def flc(costs): # ищем цель с наименьшей стоимостью
 
 
 
-node = flc(costs) # ключ(цель) с самой низкой стоимостью
+node = flc(costs) # ключ(node) с самой низкой стоимостью
 
-while node is not None:
-     cost = costs[node]
-     neig = g[node] # словарь соседей узла с наименьшим costs (А, fin)
+while node:
+
+     cost = costs[node] #стоимость узла с наименьшей стоимостью
+
+     neig = g[node] #  соседи узла с наименьшим costs (А, fin)
+
      for i in neig.keys(): # проверка cost от node до соседей( A и fin)
+
          new_cost = cost + neig[i] #сумма costs (start->node) + (node->A и fin)
-         if costs[i] > new_cost: # если (start->A) больше (start->node) + (node->A и fin
+
+         if new_cost < costs[i]: # если (start->A) больше (start->node) + (node->A и fin
+
              costs[i] = new_cost #то costs A меняем на (start->node) + (node->A и fin
+
              pars[i] = node # назначаем для A или fin нового родителя node
+             
+
      processed.append(node) # node добавляем в список обработанных
      node = flc(costs) #
+
+i = 'fin'
+while i != 'start':
     
+    j = pars[i]
+    path.append(j)
+    i = pars[i]    
 print(costs['fin'])
+
+print (path[:])
+
+
+
+
+    
     
