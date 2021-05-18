7253c4b7d49b3ede1ff690e7488b5e3108ff799f
nikulinyura <nikulin.yura@gmail.com>
2021-05-18 18:11:25 +0300

    11

---
+++ PY_intro/1num_exmpl.py
@@ -0,0 +1,44 @@
+# int 1234, -24, 0 int
+#float 1.23, 1., 
+#bin Oo177, 0x9ff
+print(2+3, 4-2, 6*3, 9/3, 4>>1, 4&3)
+
+
+print(40+3.13)
+print(int(3.44), float(4))
+a=3
+b =4
+z = (a+1, a-1)
+print(z)
+print(a+1, a-1)
+print(9%4, 4**2)
+print(10//3, 10/4)
+
+import math
+print(math.floor(2.5), math.floor(-2.5)) # округление вниз
+
+print(math.trunc(2.5), math.trunc(2.5)) # усечение до цеого
+
+print(math.pi, math.e) # константы
+
+print(math.sin(2*math.pi/180), math.sqrt(144))
+
+print(pow(2, 4)) # возведение в степень
+print(abs(-23.0), sum((1, 2, 49, 3))) # абсолютное значение, сумма
+
+print(min(1,3,5,2), max(4,2,5,7)) #минумум и максимум
+
+import random
+print(random.random())
+print(random.randint(1,10))
+print(random.choice([1,5,8]))
+
+
+
+
+
+
+
+
+
+
