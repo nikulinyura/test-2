b95b6e9d507d5546304fc4a61986adf4a1e7fedb
nikulinyura <nikulin.yura@gmail.com>
2021-05-18 14:25:32 +0300

    1

---
+++ PY_intro/num_exmpl.py
@@ -0,0 +1,16 @@
+# int 1234, -24, 0 int
+#float 1.23, 1., 
+#bin Oo177, 0x9ff
+print(2+3, 4-2, 6*3, 9/3, 4>>1, 4&3)
+
+
+print(40+3.13)
+print(int(3.44), float(4))
+a=3
+b =4
+z = (a+1, a-1)
+print(z)
+print(a+1, a-1)
+print(9%4, 4**2)
+
+
